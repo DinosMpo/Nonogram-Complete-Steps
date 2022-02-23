29e1c44615a73227f05727a4d4ad0d4f9f5dd92a
BraVe <braveheart7591@yahoo.gr>
2022-02-23 21:23:42 +0200

    create the file home.js

---
+++ client/js/home.js
@@ -0,0 +1,22 @@
+"use strict";
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+
+let nonogram = new Nonogram( [[1,0,1,0,1],
+		                        [0,1,0,1,0],
+		                        [1,0,1,0,1],
+		                        [0,1,0,1,0],
+		                        [1,0,1,0,1]    ]);
+
+canvas.width = nonogram.width;
+canvas.height = nonogram.height;
+nonogram.drawGrid();
+nonogram.drawRowNumbers();
+nonogram.drawColumnNumbers();
+
+console.log("rowNumbers: " + nonogram.rowNumbers);
+console.log("columnNumbers: "+ nonogram.columnNumbers);
+console.log("blockSize: " + nonogram.blockSize);
+console.log("width: " + nonogram.width);
+console.log("height: " + nonogram.height);
