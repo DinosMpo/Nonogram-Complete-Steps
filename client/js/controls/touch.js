2081341c8dc6f2c298fe88d6ef397dc2192d6e4b
BraVe <braveheart7591@yahoo.gr>
2022-02-27 15:38:27 +0200

    create the folder controls and the files mouse and touch js
    add only the mousedown and touchstart events

---
+++ client/js/controls/touch.js
@@ -0,0 +1,4 @@
+$(canvas).on('touchstart', function(event) {
+	startPointTouchX = Math.floor(event.touches[0].clientX - ((window.innerWidth - canvas.width)/2));
+	startPointTouchY = Math.floor(event.touches[0].clientY - ((window.innerHeight - canvas.height)/2));
+});
