8d39843fbc83e32c74a5230307b9d846e0682bc0
BraVe <braveheart7591@yahoo.gr>
2022-02-28 18:36:19 +0200

    Revert "create the stages and the functionality of them"
    This reverts commit 4641c2ec7011412c9f6faf21975f83d56d1dc882.

--- client/js/main-menu.js
+++ client/js/main-menu.js
@@ -32,25 +32,3 @@ $("#close-instructions").click(function() {
     $(this).parent().hide();
     $('#menu').show();
 });
-
-$(".close-levels5x5").click(function(){
-    $("#levels5x5").hide();
-    $("#levels").show();
-});
-
-$(".close-levels10x10").click(function(){
-    $("#levels10x10").hide();
-    $("#levels").show();
-});
-
-$(".close-levels15x15").click(function(){
-    $("#levels15x15").hide();
-    $("#levels").show();
-});
-
-$(".stage").click(function(){
-    $("#levels5x5").hide();
-    $("#levels10x10").hide();
-    $("#levels15x15").hide();
-    $("#container-tools").show();
-});
