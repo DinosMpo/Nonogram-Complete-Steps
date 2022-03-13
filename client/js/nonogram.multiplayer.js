f6f128ab8dd7958c9ba3a27c312367d5db43df69
BraVe <braveheart7591@yahoo.gr>
2022-03-13 14:09:57 +0200

    create the correct multiplayer tag and the functionality of the multiplayer stages
    code of the section 11.2

--- client/js/nonogram.multiplayer.js
+++ client/js/nonogram.multiplayer.js
@@ -4,3 +4,46 @@ const sock = io();
 sock.on('refresh counter', (data) => {
     $('#clients-count').text(data.description);
 });
+
+$('#yes').click(function() {
+    $('#'+ player +'-choice').addClass('choice-yes');
+    let data = {
+        player: player,
+        choice: 'yes'
+    };
+    if(player == 'player1') {
+        player1Choice = 'yes';
+    }else if(player == 'player2Choice') {
+        player2Choice = 'yes';
+    }
+    sock.emit('choice', data);
+});
+
+$('#no').click(function() {
+    $('#'+ player +'-choice').addClass('choice-no');
+    let data = {
+        player: player,
+        choice: 'no'
+    };
+    if(player == 'player1') {
+        player1Choice = 'no';
+    }else if(player == 'player2Choice') {
+        player2Choice = 'no';
+    }
+    sock.emit('choice', data);
+});
+
+$("#exit-multiplayer").click(function(){
+    $("#container-tools").hide();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    container.style.transform = "none";
+    container.style.left = "0%";
+    container.style.top = "0%";
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+    canvas.style.border = "none";
+    state = "menu";
+    $("#correct-multiplayer").hide();
+    $("#levels").show();
+    $("#clients-count").show();
+});
